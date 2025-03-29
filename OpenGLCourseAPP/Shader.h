83d20608edcc05e2341b115a0ef04f00e460673a
Danny López Cárdenas <danny.lopezc00@gmail.com>
2025-03-29 11:18:12 -0500

    Cleaning up

---
+++ OpenGLCourseAPP/Shader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+	Shader();
+
+	void createFromString(const char* vertexCode, const char* fragmentCode);
+	void createFromFiles(const char* vertexLocation, const char* fragmentLocation);
+
+	std::string ReadFile(const char* location);
+
+	GLuint getProjectionLocation();
+	GLuint getModelLocation();
+
+	void useShader();
+
+	~Shader();
+
+private:
+
+	GLuint shaderId, uniformProjection, uniformModel;
+
+	void compileShader(const char* vertexCode, const char* fragmentCode);
+	void addShader(GLuint theProgram, const char* shaderCode, GLenum shaderType);
+
+	void clearShader();
+};
+
---
+++ OpenGLCourseAPP/Window.cpp
@@ -0,0 +1,77 @@
+#include "Window.h"
+
+Window::Window()
+{
+    width = 800;
+    height = 600;
+	bufferWidth = 0;
+	bufferHeight = 0;
+	mainWindow = nullptr;
+}
+
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+    width = windowWidth;
+    height = windowHeight;
+	bufferWidth = 0;
+	bufferHeight = 0;
+	mainWindow = nullptr;
+}
+
+int Window::initialise()
+{
+	// Init GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW Init failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties
+	// OpenGL version
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core profile = No Backwords compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow forward compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	mainWindow = glfwCreateWindow(width, height, "Test window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("Window creating failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size Information
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// set context for GLEW
+	glfwMakeContextCurrent(mainWindow);
+
+	// allow mordern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Glew init failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// setup viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+    return 0;
+}
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+}
